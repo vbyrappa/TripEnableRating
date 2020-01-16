f1b728664a56bcda50ad12819d3cc3e0dba688ea
vbyrappa <vijay.byrappa@gmail.com>
2020-01-16 22:09:39 +0530

    Commit Review Edited

--- RateTrip/src/main/java/DemoTrip/RateTrip/ReviewClick.java
+++ RateTrip/src/main/java/DemoTrip/RateTrip/ReviewClick.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.interactions.Actions;import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ReviewClick {
